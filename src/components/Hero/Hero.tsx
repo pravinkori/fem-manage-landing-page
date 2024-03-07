eaf384fd1e96e0a0784cd80adb007b24a07bed84
Pravin Kori <pravinkori@protonmail.ch>
2024-03-07 13:47:46 +0530

    style: reorganize imports and add Header and Hero components to App component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,28 @@
+import heroIllustration from "/src/assets/images/illustration-intro.svg";
+import styles from "./Hero.module.css";
+import "../../css/App.css";
+
+function Hero() {
+  return (
+    <section>
+      <div className={styles.container}>
+        <div className={styles["even-columns"]}>
+          <h1 className={`${styles} fs-primary-heading fw-bold `}>
+            Bring everyone together to build better products
+          </h1>
+          <p>
+            {" "}
+            Manage makes it simple for software teams to plan day-to-day tasks
+            while keeping the larger team goals in view.
+          </p>
+          <button className={styles.button}>Get Started</button>
+        </div>
+        <div>
+          <img src={heroIllustration} alt="" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
