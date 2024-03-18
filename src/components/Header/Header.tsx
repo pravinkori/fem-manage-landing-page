8f1f89320d7fee5154c1d6f64afd86a7bef18f1d
Pravin Kori <pravinkori@protonmail.ch>
2024-03-18 22:23:37 +0530

    style: organize imports and add mobile navigation toggle button to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import logo from "/src/assets/images/logo.svg";
 import styles from "./Header.module.css";
-import "/src/css/App.css";
 import Button from "../Button/Button";
+import closeIcon from "/src/assets/images/icon-close.svg";
+import hamburgerIcon from "/src/assets/images/icon-hamburger.svg";
 
 function Header() {
   return (
@@ -11,8 +12,31 @@ function Header() {
           <a href="#">
             <img src={logo} alt="Manage" />
           </a>
+          <button
+            className={styles["mobile-nav-toggle"]}
+            aria-controls="primary-navigation"
+          >
+            <img
+              className={styles["icon-hamburger"]}
+              src={hamburgerIcon}
+              alt=""
+              aria-hidden="true"
+            />
+            <img
+              className={styles["icon-close"]}
+              src={closeIcon}
+              alt=""
+              aria-hidden="true"
+            />
+            <span className="visually-hidden">Menu</span>
+          </button>
           <nav className={styles["primary-navigation"]}>
-            <ul aria-label="Primary" role="list" className={styles["nav-list"]}>
+            <ul
+              aria-label="Primary"
+              role="list"
+              className={styles["nav-list"]}
+              id="primary-navigation"
+            >
               <li>
                 <a href="#">Pricing</a>
               </li>
