0022bcabeaaeefc140d56d0fcac9b201ae48b2a3
Pravin Kori <pravinkori@protonmail.ch>
2024-03-24 17:39:17 +0530

    style: update text width for paragraphs in specific sections to improve readability

--- src/components/Features/Feature.tsx
+++ src/components/Features/Feature.tsx
@@ -22,10 +22,10 @@ function Feature() {
                   className={`flow`}
                   style={{ "--flow-spacer": `1em` } as React.CSSProperties}
                 >
-                  <h3 className={`fs-600 fw-bold`}>
+                  <h3 className={`ff-accent fs-600 fw-bold`}>
                     Track company-wide progress
                   </h3>
-                  <p>
+                  <p data-width="wide">
                     See how your day-to-day tasks fit into the wider vision. Go
                     from tracking progress at the milestone level all the way
                     done to the smallest of details. Never lose sight of the
@@ -38,10 +38,10 @@ function Feature() {
                   className={`flow`}
                   style={{ "--flow-spacer": `1em` } as React.CSSProperties}
                 >
-                  <h3 className={`fs-600 fw-bold`}>
+                  <h3 className={`ff-accent fs-600 fw-bold`}>
                     Advanced built-in reports
                   </h3>
-                  <p>
+                  <p data-width="wide">
                     Set internal delivery estimates and track progress toward
                     company goals. Our customisable dashboard helps you build
                     out the reports you need to keep key stakeholders informed.
@@ -53,10 +53,10 @@ function Feature() {
                   className={`flow`}
                   style={{ "--flow-spacer": `1em` } as React.CSSProperties}
                 >
-                  <h3 className={`fs-600 fw-bold`}>
+                  <h3 className={`ff-accent fs-600 fw-bold`}>
                     Everything you need in one place
                   </h3>
-                  <p>
+                  <p data-width="wide">
                     Stop jumping from one service to another to communicate,
                     store files, track tasks and share documents. Manage offers
                     an all-in-one team productivity solution.
